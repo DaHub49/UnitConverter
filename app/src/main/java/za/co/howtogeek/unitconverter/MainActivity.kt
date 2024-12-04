2cc5963f3b3527c337e069f3f6ce6ffa5c766278
DaHub49 <dyls.martin.49@gmail.com>
2024-12-04 07:20:24 +0200

    04/12/2024 @07:20 [video 82]:
    Added a Button and onClick() function.
    Listened to an explanation of Context and LocalContext.current for use in a Toast.

--- app/src/main/java/za/co/howtogeek/unitconverter/MainActivity.kt
+++ app/src/main/java/za/co/howtogeek/unitconverter/MainActivity.kt
@@ -1,17 +1,20 @@
 package za.co.howtogeek.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import za.co.howtogeek.unitconverter.ui.theme.UnitConverterTheme
 
@@ -55,10 +58,16 @@ fun UnitConverter(){
         Row {
 
         }
-        Text("Result:")
         Row {
-            Text("[Result]")
+            val context = LocalContext.current
+            Button(onClick = {
+                Toast.makeText(context,
+                    "Thanks for clicking, click again!",
+                    Toast.LENGTH_LONG).show() }) {
+                Text("Click Me!")
+            }
         }
+        Text("[Result]")
     }
 }
 
