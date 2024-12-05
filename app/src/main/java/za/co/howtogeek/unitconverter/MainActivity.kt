fcd8c49344681612c120a9794a6736eeb415801d
DaHub49 <dyls.martin.49@gmail.com>
2024-12-05 11:49:00 +0200

    05/12/2024 @11:48: [video 86]:
    Added a DropDownMenu and multiple DropDownMenuItems and added a Spacer between the 2 buttons.

--- app/src/main/java/za/co/howtogeek/unitconverter/MainActivity.kt
+++ app/src/main/java/za/co/howtogeek/unitconverter/MainActivity.kt
@@ -10,10 +10,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -42,57 +44,103 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-// Create new Composable with Columns:
-@Composable
-fun UnitConverter(){
-    Column (// modifier property inside the normal brackets:
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-                ) {
+    // Create new Composable with Columns:
+    @Composable
+    fun UnitConverter() {
+        Column(// modifier property inside the normal brackets:
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-        // Here all the UI elements will b e stacked below each other:
-        /* There are 3 different types of text fields:
-           (1) Text("Unit Converter")
-           (2) BasicTextField()
-           (3) OutlinedTextField()
-         */
-        Text("Unit Converter")
-            //modifier = Modifier.padding(100.dp))
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = "", onValueChange = {
-            /*
-            {} implement logic here if needed
-            Anonymous functions are single-use functions that don't have names.
+            // Here all the UI elements will b e stacked below each other:
+            /* There are 3 different types of text fields:
+               (1) Text("Unit Converter")
+               (2) BasicTextField()
+               (3) OutlinedTextField()
              */
-        })
-        Spacer(modifier = Modifier.height(16.dp))
-        // Row: Here all the elements will be stacked alongside each other:
-        Row {
-            Box{
-                Button(onClick = { /*TODO*/ }){
-                    Text("Select")
-                    Icon(Icons.Default.ArrowDropDown,
-                        contentDescription = "Arrow Down") // contentDescription for accessibility
+            Text("Unit Converter")
+            //modifier = Modifier.padding(100.dp))
+            Spacer(modifier = Modifier.height(16.dp))
+            OutlinedTextField(value = "", onValueChange = {
+                /*
+                {} implement logic here if needed
+                Anonymous functions are single-use functions that don't have names.
+                 */
+            })
+            Spacer(modifier = Modifier.height(16.dp))
+            // Row: Here all the elements will be stacked alongside each other:
+            Row {
+                Box {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text("Select")
+                        Icon(
+                            Icons.Default.ArrowDropDown,
+                            contentDescription = "Arrow Down"
+                        ) // contentDescription for accessibility
+                    }
+                    DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                        DropdownMenuItem(
+                            text = { Text("Centimetres") },
+                            onClick = { /*TODO*/ }
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Metres") },
+                            onClick = { /*TODO*/ }
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Feet") },
+                            onClick = { /*TODO*/ }
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Millimetres") },
+                            onClick = { /*TODO*/ }
+                        )
+                    }
                 }
-            }
-            Box{
-                Button(onClick = { /*TODO*/ }){
-                    Text("Select")
-                    Icon(Icons.Default.ArrowDropDown,
-                        contentDescription = "Arrow Down") // contentDescription for accessibility
+
+                // Add a Spacer between the 2 boxes:
+                Spacer(modifier = Modifier.width(16.dp))
+
+
+
+                Box {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text("Select")
+                        Icon(
+                            Icons.Default.ArrowDropDown,
+                            contentDescription = "Arrow Down"
+                        ) // contentDescription for accessibility
+                    }
+                    DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                        DropdownMenuItem(
+                            text = { Text("Centimetres") },
+                            onClick = { /*TODO*/ }
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Metres") },
+                            onClick = { /*TODO*/ }
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Feet") },
+                            onClick = { /*TODO*/ }
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Millimetres") },
+                            onClick = { /*TODO*/ }
+                        )
+                    }
                 }
+                Spacer(modifier = Modifier.height(16.dp))
+                Text("[Result]")
             }
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        Text("[Result]")
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun UnitConverterPreview(){
-    UnitConverter()
+    @Preview(showBackground = true)
+    @Composable
+    fun UnitConverterPreview() {
+        UnitConverter()
+    }
 }
